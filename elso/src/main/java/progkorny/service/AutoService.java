8b8fff46502ec3dff2c7a4dfec57d07cc1e86e04
Tamás Judit <judit0310@gmail.com>
2019-11-28 14:49:54 +0100

    Mocking with matcher

--- elso/src/main/java/progkorny/service/AutoService.java
+++ elso/src/main/java/progkorny/service/AutoService.java
@@ -3,6 +3,7 @@ package progkorny.service;
 import model.exceptions.RosszRendszam;
 import progkorny.dao.IAutoDAO;
 import progkorny.model.Auto;
+import progkorny.model.Uzemanyag;
 import progkorny.model.exceptions.AutoNotFound;
 import progkorny.model.exceptions.DuplikaltAuto;
 
@@ -35,13 +36,28 @@ public class AutoService {
     Collection<Auto> getAllAutoFromYear(int year) throws IOException {
         Collection<Auto> result = new ArrayList<>();
         Collection<Auto> autok = readAllAuto();
-        for (Auto a:autok
-             ) {
-            if (a.getGyartasi_ido().getYear()==year){
+        for (Auto a : autok
+        ) {
+            if (a.getGyartasi_ido().getYear() == year) {
                 result.add(a);
             }
 
         }
+        return result;
+
+    }
+
+    Collection<Auto> getAutoByFuel(Uzemanyag uzemanyag) throws IOException {
+        Collection<Auto> autok = dao.readAllAuto();
+        Collection<Auto> result = new ArrayList<>();
+        for (Auto a : autok
+        ) {
+            if(a.getUzemanyag() == uzemanyag){
+                result.add(a);
+            }
+
+        }
+
         return result;
 
     }
--- elso/src/test/java/progkorny/service/AutoServiceTest.java
+++ elso/src/test/java/progkorny/service/AutoServiceTest.java
@@ -1,11 +1,16 @@
 package progkorny.service;
 
+import model.exceptions.RosszRendszam;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.AdditionalMatchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import progkorny.dao.IAutoDAO;
 import progkorny.model.Auto;
 import progkorny.model.Uzemanyag;
+import progkorny.model.exceptions.AutoNotFound;
 import progkorny.model.exceptions.DuplikaltAuto;
 
 import java.io.IOException;
@@ -21,14 +26,25 @@ public class AutoServiceTest {
 
 
     @Before
-    public void setUp() throws Exception, DuplikaltAuto {
+    public void setUp() throws Exception, DuplikaltAuto, AutoNotFound, RosszRendszam {
         IAutoDAO mock= Mockito.mock(IAutoDAO.class);
         service= new AutoService(mock);
         Auto auto = new Auto("Ford", "Focus", "ABC-123", "feher", Uzemanyag.BENZIN, LocalDate.of(2014,02,15) );
+        Auto auto2 = new Auto("Opel", "Corsa", "AAA-111", "fekete", Uzemanyag.BENZIN, LocalDate.of(2011,12,19) );
+
         Collection<Auto> autok = new ArrayList<>();
         autok.add(auto);
+        autok.add(auto2);
         Mockito.when(mock.readAllAuto()).thenReturn(autok);
         Mockito.doThrow(DuplikaltAuto.class).when(mock).addAuto(auto);
+
+
+        Mockito.doThrow(RosszRendszam.class).when(mock).readAutoByRenszam(
+                AdditionalMatchers.not
+                        (Mockito.matches("\\w\\w\\w-\\d\\d\\d")));
+        Mockito.doThrow(AutoNotFound.class).when(mock).readAutoByRenszam(Mockito.matches("\\w\\w\\w-\\d\\d\\d"));
+        Mockito.doReturn(auto).when(mock).readAutoByRenszam("ABC-123");
+
 
     }
 
@@ -41,7 +57,33 @@ public class AutoServiceTest {
     @Test(expected = DuplikaltAuto.class)
     public void testDuplikalt() throws IOException, DuplikaltAuto {
         Auto auto = new Auto("Ford", "Focus", "ABC-123", "feher", Uzemanyag.BENZIN, LocalDate.of(2014,02,15) );
-
         service.addAuto(auto);
+    }
+
+    @Test
+    public void testReadByRendszamWorking() throws RosszRendszam, AutoNotFound, IOException {
+        System.out.println(service.readAutoByRenszam("ABC-123"));
+    }
+
+    @Test(expected = AutoNotFound.class)
+    public void testReadByRendszamException() throws RosszRendszam, AutoNotFound, IOException {
+        service.readAutoByRenszam("AAA-111");
+    }
+
+    @Test(expected = RosszRendszam.class)
+    public void testReadByRendszamWrongRendszam() throws RosszRendszam, AutoNotFound, IOException {
+        service.readAutoByRenszam("AA-111");
+    }
+
+    @Test
+    public void getCorrectFuelSize() throws IOException {
+        assertEquals(2, service.getAutoByFuel(Uzemanyag.BENZIN).size());
+        assertEquals(0, service.getAutoByFuel(Uzemanyag.DIESEL).size());
+        assertEquals(service.readAllAuto().size(), service.getAutoByFuel(Uzemanyag.BENZIN).size()+service.getAutoByFuel(Uzemanyag.DIESEL).size());
+        Collection<Auto> autok = service.readAllAuto();
+        for (Auto a: service.getAutoByFuel(Uzemanyag.BENZIN)){
+            assertThat(autok, Matchers.hasItem(a));
+        }
+        assertThat(autok, Matchers.contains(service.getAutoByFuel(Uzemanyag.BENZIN).toArray()));
     }
 }
